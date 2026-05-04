8eb8f56821c2a0606c9454379e35237077de36c0
vibek44 <kshetrivibek@gmail.com>
2026-05-04 11:15:28 +0300

    Adding error handler for mongodb unique indexing(MongoServerError) and jsonWebTokenError

--- utilities/middleware.js
+++ utilities/middleware.js
@@ -9,6 +9,12 @@ const errorHandler = (error,req,res,next) => {
   if (error.name==='ValidationError'){
     return res.status(400).json({ error:error.message })
   }
+  if(error.name==='MongoServerError' && error.message.includes('E11000 duplicate key error')){
+    return res.status(400).json({error:'expected `userName` to be unique'})
+  }
+  if(error.name==='JsonWebTokenError'){
+    return res.status(401).json({error:'token invalid'})
+  }
   next(error)
 }
 
