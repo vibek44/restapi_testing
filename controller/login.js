072f4a7e313af720e3872b4f48c9b9f12a7e3e1c
vibek44 <kshetrivibek@gmail.com>
2026-05-04 11:10:56 +0300

    adding login controller for login functionality

---
+++ controller/login.js
@@ -0,0 +1,25 @@
+const jwt=require('jsonwebtoken')
+const bcrypt=require('bcrypt')
+const User=require('../models/user')
+const loginRouter=require('express').Router()
+
+loginRouter.post('/', async(req,res)=>{
+  const {userName,password}=req.body
+  const user=await User.findOne({userName})
+  const passwordCorrect=user===null
+    ? false
+    : await bcrypt.compare(password,user.passwordHash)
+
+  if(!( user && passwordCorrect)){
+    res.status(401).json({error:'invalid username or password'})
+  }
+  const userForToken={
+      userName:user.userName,
+      id:user._id
+  }
+  const token=jwt.sign(userForToken,process.env.SEKRET,{expiresIn:60*60})
+
+  res.status(200).send({token,userName:user.userName,id:user._id})
+})
+
+module.exports=loginRouter
