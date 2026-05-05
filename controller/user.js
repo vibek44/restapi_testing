9cf79b0703f034ab931442d781770fb3252d424d
vibek44 <kshetrivibek@gmail.com>
2026-05-05 09:31:40 +0300

    adding functionality for user registration validation

--- controller/user.js
+++ controller/user.js
@@ -4,22 +4,27 @@ const User=require('../models/user')
 const bcrypt=require('bcrypt')
 
 userRouter.get('/', async (req,res)=>{
-  console.log(req)
-   const users=await User.find({}).populate('blogs',{title:1,author:1,url:1})
-   res.json(users)
+  //console.log(req)
+  const users=await User.find({}).populate('blogs',{title:1,author:1,url:1})
+  res.json(users)
 })
 
 userRouter.post('/', async (req,res) => {
   
   const { userName,name,password } = req.body 
-
+  if(!userName || !password ){
+    return res.status(400).send({error:'username or password missing'})
+  }
+  if(!(userName.length>=3) || !(password.length>=3)){
+    return res.status(400).send({error:'userName or password length must be greater than 3'})
+  } 
   const saltRounds=10
   const passwordHash=await bcrypt.hash(password, saltRounds)
 
   const user=new User({
-      userName,
-      name,
-      passwordHash
+    userName,
+    name,
+    passwordHash
   })
 
   const savedUser=await user.save()
