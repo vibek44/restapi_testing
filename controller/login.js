c97fda591d46edaf97c204ed2dd92fc0097c12d8
vibek44 <kshetrivibek@gmail.com>
2026-05-05 09:55:33 +0300

    adding functionality to check valid user in login

--- controller/login.js
+++ controller/login.js
@@ -3,23 +3,26 @@ const bcrypt=require('bcrypt')
 const User=require('../models/user')
 const loginRouter=require('express').Router()
 
-loginRouter.post('/', async(req,res)=>{
-  const {userName,password}=req.body
-  const user=await User.findOne({userName})
+loginRouter.post('/', async(req,res) => {
+  const { userName,password }=req.body
+  if( !userName || !password || !(userName.length>=3) || !(password.length>=3)){
+    return res.status(400).json({error:'invalid username or password'})
+  }
+  const user=await User.findOne({ userName })
   const passwordCorrect=user===null
     ? false
     : await bcrypt.compare(password,user.passwordHash)
 
   if(!( user && passwordCorrect)){
-    res.status(401).json({error:'invalid username or password'})
+    return res.status(401).json({ error:'invalid username or password' })
   }
   const userForToken={
-      userName:user.userName,
-      id:user._id
+    userName:user.userName,
+    id:user._id
   }
-  const token=jwt.sign(userForToken,process.env.SEKRET,{expiresIn:60*60})
+  const token=jwt.sign(userForToken,process.env.SEKRET,{ expiresIn:60*60 })
 
-  res.status(200).send({token,userName:user.userName,id:user._id})
+  res.status(200).send({ token,userName:user.userName,id:user._id })
 })
 
 module.exports=loginRouter
