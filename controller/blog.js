2bf824e21c069c10aaa912b7997d3db1c34e08b1
vibek44 <kshetrivibek@gmail.com>
2026-05-05 09:38:53 +0300

    adding token authentication  functionality for creating blog act

--- controller/blog.js
+++ controller/blog.js
@@ -1,13 +1,16 @@
 
 const blogRouter=require('express').Router( { caseSensitive:true,strict:true } )
-const res = require('express/lib/response')
 const  mongoose  = require('mongoose')
 const Blog=require('../models/blog')
+const User=require('../models/user')
+const jwt=require('jsonwebtoken')
 
 blogRouter.get('/', async (req,res) => {
-  const blogs=await Blog.find({})
+  const blogs=await Blog.find({}).populate('user', { userName:1, name:1 })
+  //console.log(req.get('authorization'))
   res.json(blogs)
 })
+
 blogRouter.get('/:id', async (req,res) => {
   const blog=await Blog.findById( req.params.id )
   if(blog){
@@ -16,12 +19,12 @@ blogRouter.get('/:id', async (req,res) => {
     res.status(204).end()
   }
 })
+
 blogRouter.put('/:id', async(req,res) => {
   const blog=await Blog.findById( req.params.id )
   if(!blog){
     return res.status(404).end()
   }
-
   blog.title=req.body.title
   blog.url=req.body.url
   blog.likes=req.body.likes
@@ -31,17 +34,30 @@ blogRouter.put('/:id', async(req,res) => {
   res.json(updatedBlog)
 })
 
+
 blogRouter.post('/', async (req,res) => {
-  if(!req.body.title || !req.body.url){
+  const body=req.body
+  console.log(req.token)
+  const decodedToken=jwt.verify(req.token,process.env.SEKRET)
+  if(!decodedToken.id){
+    return res.status(401).send({error:'invalid token'})
+  }
+  const user=await User.findById(decodedToken.id)
+  if(!user){
+    return res.status(400).json({error:'userId missing or invalid'})
+  }
+  if(!body.title || !body.url){
     return res.status(400).end()
   }
 
-  if(!req.body.likes){
-    req.body.likes=0
+  if(!body.likes){
+    body.likes=0
   }
-  const blog=new Blog(req.body)
-  const result=await blog.save()
-  res.status(201).json(result)
+  const blog=new Blog({...body,user:user._id})
+  const savedBlog=await blog.save()
+  user.blogs=user.blogs.concat(savedBlog._id)
+  await user.save()
+  res.status(201).json(savedBlog)
 
 })
 
