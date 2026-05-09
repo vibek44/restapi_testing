85c10d93b1f26a1448f1f7b5bf47c2d72a94a836
vibek44 <kshetrivibek@gmail.com>
2026-05-09 14:28:07 +0300

    fixing test functinality by adding login functionality and setting authorization header in all unit test

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,9 @@
-const { test,after,beforeEach,describe,runOnly }=require('node:test')
+const { test,after,beforeEach,describe,only }=require('node:test')
 const  assert =require('node:assert').strict
 const mongoose=require('mongoose')
 const supertest=require('supertest')
 const app=require('../app')
+const User=require('../models/user')
 const Blog=require('../models/blog')
 const { initialBlog,blogsInDB }=require('./api_test_helper')
 
@@ -10,13 +11,30 @@ const api=supertest(app)
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-  const blogObjects=initialBlog.map(blog => new Blog(blog))
+  const response=await api
+    .post('/api/login')
+    .send({ userName:'bhim123',password:'Sekret' })
+    .expect(200)
+    .expect('Content-Type',/application\/json/)
+
+  const user=await User.findById(response.body.id)
+  user.blogs=[]
+  await user.save()
+  const blogObjects=initialBlog.map(blog => new Blog( { ...blog,user:user._id }) )
+  for (const blogel of blogObjects){
+    const savedBlog=await blogel.save()
+    user.blogs=user.blogs.concat(savedBlog._id)
+    await user.save()
+  }
+  /*
   const promiseArray=blogObjects.map(blog => blog.save())
+  console.log(promiseArray)
   await Promise.all(promiseArray)
+  */
 })
 
-describe('HTTP GET request testing for blog api', ()=>{
-  test('notes are returned as json', async () => {
+describe('HTTP GET request testing for blog api', () => {
+  test('Blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
       .expect(200)
@@ -29,7 +47,7 @@ describe('HTTP GET request testing for blog api', ()=>{
   })
 })
 
-describe('HTTP post request test',() =>{
+describe('HTTP post request test',() => {
   test('HTTP post request creates successful blog post', async () => {
     const newBlog={
       title: 'my name is why',
@@ -37,16 +55,23 @@ describe('HTTP post request test',() =>{
       url: 'https://www.bibliofreak.net/2021/09/review-my-name-is-why-by-lemn-sissay.html',
       likes: 10
     }
-
+    const response=await api
+      .post('/api/login')
+      .send({ userName:'bhim123',password:'Sekret' })
+    const token= response.body.token
     await api
       .post('/api/blogs')
+      .set('Authorization',`Bearer ${token}`)
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/)
 
     const blogAtEnd=await blogsInDB()
     const addedBlog=blogAtEnd.find(blog => blog.title===newBlog.title)
+
     delete addedBlog.id
+    addedBlog.user=addedBlog.user.toString()
+    newBlog.user=response.body.id
     assert.deepStrictEqual(addedBlog,newBlog)
     /*
     assert.strictEqual(newBlog.title, addedBlog.title)
@@ -64,9 +89,14 @@ describe('HTTP post request test',() =>{
       author: 'Arthur Lim',
       url: 'https://www.bibliofreak.net/2021/09/review-my-name-is-why-by-lemn-sissay.html',
     }
+    const response=await api
+      .post('/api/login')
+      .send({ userName:'bhim123',password:'Sekret' })
+    const token= response.body.token
 
     await api
       .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/)
@@ -83,22 +113,33 @@ describe('HTTP post request test',() =>{
       author: 'Arthur Lim',
       url: 'https://www.bibliofreak.net/2021/09/review-my-name-is-why-by-lemn-sissay.html',
     }
+
     const response=await api
+      .post('/api/login')
+      .send({ userName:'bhim123',password:'Sekret' })
+
+    const token= response.body.token
+    const response2=await api
       .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
       .send(newBlog)
       .expect(400)
 
-    assert.strictEqual(response.res.statusMessage, 'Bad Request')
+    assert.strictEqual(response2.res.statusMessage, 'Bad Request')
 
   })
 })
-describe('HTTP DELETE test for blog api', ()=>{
+describe('HTTP DELETE test for blog api', () => {
   test('Deleting single blog resource is successful', async() => {
     const blogsAtStart=await blogsInDB()
     const blogToDelete=blogsAtStart[0]
-
+    const response=await api
+      .post('/api/login')
+      .send({ userName:'bhim123',password:'Sekret' })
+    const token= response.body.token
     await api
       .delete(`/api/blogs/${blogToDelete.id}`)
+      .set('Authorization', `Bearer ${token}`)
       .expect(204)
 
     const blogsAtEnd=await blogsInDB()
@@ -109,13 +150,17 @@ describe('HTTP DELETE test for blog api', ()=>{
   })
 })
 describe.only('HTTP PUT request test', () => {
-  test('functionality for updating the information about individual blog post is correct', async() => {
+  test.only('functionality for updating the information about individual blog post is correct', async() => {
     const blogsAtStart=await blogsInDB()
     const blogToEdit=blogsAtStart[0]
     blogToEdit.likes=20
-
+    const response=await api
+      .post('/api/login')
+      .send({ userName:'bhim123',password:'Sekret' })
+    const token= response.body.token
     await api
       .put(`/api/blogs/${blogToEdit.id}`)
+      .set('Authorization', `Bearer ${token}`)
       .send(blogToEdit)
       .expect(200)
 
@@ -129,4 +174,5 @@ describe.only('HTTP PUT request test', () => {
 
 after(async () => {
   await mongoose.connection.close()
+
 })
