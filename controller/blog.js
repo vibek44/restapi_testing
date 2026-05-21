03c6012bb67054bf0e7b0f077ae0a23c8f93a6c6
vibek44 <kshetrivibek@gmail.com>
2026-05-21 19:04:08 +0300

    changing response object data structure for put and post request to end point

--- controller/blog.js
+++ controller/blog.js
@@ -7,7 +7,7 @@ const jwt=require('jsonwebtoken')
 const { userExtractor }=require('../utilities/middleware')
 
 blogRouter.get('/', async (req,res) => {
-  const blogs=await Blog.find({}).populate('user', { userName:1, name:1 })
+  const blogs=await Blog.find({}).populate('user',{userName:1,name:1})
   res.json(blogs)
 })
 
@@ -20,26 +20,29 @@ blogRouter.get('/:id', async (req,res) => {
   }
 })
 
-blogRouter.put('/:id',userExtractor ,async(req,res) => {
+blogRouter.put('/:id',userExtractor, async(req,res) => {
+  const body=req.body
   const blog=await Blog.findById(req.params.id)
-  const user=req.user
-
+  
   if(!blog){
     return res.status(404).end()
   }
-  /*
-  if(blog.user.toString()!==user._id.toString()){
-    return res.status(401).json({ error:'unauthorized user' })
-  }
-  */
-  blog.title=req.body.title
-  blog.url=req.body.url
-  blog.likes=req.body.likes
-  blog.author=req.body.author
+  blog.title=body.title
+  blog.url=body.url
+  blog.likes=body.likes
+  blog.author=body.author
 
   const updatedBlog=await blog.save()
   
-  res.json(updatedBlog)
+  res.json({ 
+    id:updatedBlog._id,
+    title:updatedBlog.title,
+    author:updatedBlog.author,
+    url:updatedBlog.url,
+    likes:updatedBlog.likes,
+    user:{userName:body.user.userName,name:body.user.name,id:body.id}
+    
+  })
 })
 
 
@@ -54,21 +57,29 @@ blogRouter.post('/',userExtractor, async (req,res) => {
     body.likes=0
   }
   const blog=new Blog({ ...body,user:user._id })
-  const savedBlog=await blog.save()
+  let savedBlog=await blog.save()
   user.blogs=user.blogs.concat(savedBlog._id)
   await user.save()
-  res.status(201).json(savedBlog)
+  res.status(201).json({ 
+    id:savedBlog._id,
+    title:savedBlog.title,
+    author:savedBlog.author,
+    url:savedBlog.url,
+    likes:savedBlog.likes,
+    user:{userName:user.userName,name:user.name,id:user._id}
+    
+  })
 
 })
 
 blogRouter.delete('/:id',userExtractor ,async(req,res) => {
   const user=req.user
+  //console.log('from rou',user)
   const blog=await Blog.findById(req.params.id)
   if(!blog){
-
     return res.status(400).json({ error:'Bad Request' })
   }
-  if(user.id.toString()!==blog.user.toString()){
+  if(user._id.toString()!==blog.user.toString()){
     return res.status(401).send({ error:'unauthorized operation' })
   }
   user.blogs=user.blogs.filter(blog => blog.toString()!==req.params.id)
